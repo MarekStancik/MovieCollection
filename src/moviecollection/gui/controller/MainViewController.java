dd4ad881e1bb7e12ad14b6be3c7524bb636db45f
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2019-01-09 13:21:36 +0100

    Gui part

--- src/moviecollection/gui/controller/MainViewController.java
+++ src/moviecollection/gui/controller/MainViewController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package moviecollection;
+package moviecollection.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +11,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+import moviecollection.be.Category;
+import moviecollection.be.Movie;
 
 /**
  *
@@ -19,10 +26,24 @@ import javafx.scene.control.Label;
 public class MainViewController implements Initializable
 {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private ToggleGroup filter;
+    @FXML
+    private ListView<Category> catList;
+    @FXML
+    private ListView<Movie> movieList;
+    @FXML
+    private Label movieTitle;
+    @FXML
+    private Text movieDesc;
+    @FXML
+    private ProgressBar ratingBar;
+    @FXML
+    private Label rating;
+    
+    private ModelViewController model;
+    
     private void handleButtonAction(ActionEvent event)
     {
         System.out.println("You clicked me!");
@@ -32,7 +53,73 @@ public class MainViewController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
+        model = new ModelViewController();
         // TODO
     }    
+
+    @FXML
+    private void radioAll(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void radioOne(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void orderByTitle(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void orderByCat(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void orderByRating(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void searchCat(KeyEvent event)
+    {
+    }
+
+    @FXML
+    private void addCat(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void removeCat(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void searchMovies(KeyEvent event)
+    {
+    }
+
+    @FXML
+    private void addMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void removeMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void editMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void pressPlay(ActionEvent event)
+    {
+    }
     
 }
--- src/moviecollection/gui/controller/ModelViewController.java
+++ src/moviecollection/gui/controller/ModelViewController.java
@@ -5,11 +5,52 @@
  */
 package moviecollection.gui.controller;
 
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import moviecollection.be.Category;
+import moviecollection.be.Movie;
+import moviecollection.be.MovieFilter;
+import moviecollection.bll.BusinessModel;
+import moviecollection.bll.IModel;
+
 /**
  *
  * @author Marek
  */
 public class ModelViewController
 {
+    private ObservableList<Movie> movieList;
+    private ObservableList<Category> categoryList;
+    private IModel model;
+    
+    public ModelViewController()
+    {
+        model = new BusinessModel();
+        movieList = FXCollections.observableArrayList(model.getAllMovies());
+        categoryList = FXCollections.observableArrayList(model.getAllCategories());
+    }
+    
+    public void setMovieFilter(String filter)
+    {
+        movieList.setAll(model.getFilteredMovies(filter));
+    }
+    
+    public List<Movie> getFilteredMovies(MovieFilter filter)
+    {
+        
+    }
+    
+    public List<Movie> getMovieList();
+    List<Category> getAllCategories();
+    void addMovie(Movie m);
+    void removeMovie(Movie m);
+    void addCategory(Category c);
+    void removeCategory(Category c);
+    void setMovieCategories(Movie m,Category[] categories);
+    void setMovieRating(Movie m,short rating);
+    
     
 }
